6076ef7e56388cfedfa52ca7ec4d4eee5fe58fb1
Ctit28 <nguyenmap29082003@gmail.com>
2025-07-22 09:01:07 +0700

    up l1

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,36 +1,12 @@
 import mysql from "mysql2/promise";
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bookstore",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 export default pool;
-
-// import mysql from "mysql2/promise";
-// import dotenv from "dotenv";
-
-// dotenv.config(); // load biến môi trường từ file .env
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 3306,
-// });
-
-// export default pool;
-
-// import mysql from "mysql2/promise";
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// export default pool;
